c4fa72594bf201a226e34fb1e2e1fed5dfdc0f16
XhyEax <xhyeax@qq.com>
2021-11-23 00:45:15 +0800

    add log function

--- agent/logger.ts
+++ agent/logger.ts
@@ -1,4 +1,14 @@
-export function log(...data: any): void {
+export function log(...data: any) {
     // @ts-ignore
     console.log.apply(console, arguments);
+}
+
+export function logw(...data: any[]) {
+    // @ts-ignore
+    console.warn.apply(console, arguments);
+}
+
+export function loge(...data: any[]) {
+    // @ts-ignore
+    console.error.apply(console, arguments);
 }
